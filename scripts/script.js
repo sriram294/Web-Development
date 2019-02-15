47aa6c7228f8d6d19a6f3ee59f75a8753706b514
sriram294 <sriram294@gmail.com>
2019-02-16 02:17:13 +0530

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,17 +1,1 @@
 
-jQuery(document).ready(function($) {
-  $(window).scroll(function() {
-    var scrollPos = $(window).scrollTop(),
-        navbar = $('#navbar');
-        brand = $('.navbar-brand');
-
-    if (scrollPos > navbar.height()) {
-      navbar.addClass('nav-colored');
-      navbar.removeClass('nav-transparent');
-
-    } else {
-      navbar.addClass('nav-transparent');
-      navbar.removeClass('nav-colored');
-    }
-  });
-});
